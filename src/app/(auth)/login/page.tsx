12d64a42b29657c30fd6d66c440bc112b35a5c24
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-09 00:16:18 +0200

    create a simple login form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,5 +1,12 @@
 "use client";
-
 export default function LoginPage() {
-  return <div>Login</div>;
+  return (
+    <main>
+      <form action={"/login"}>
+        <input type="email" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </main>
+  );
 }
