6c01391b05981373dfcd08718052cede7faa6945
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-09 00:46:22 +0200

    setup a simple form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,29 @@
 "use client";
+import styles from "../auth.module.css";
 export default function LoginPage() {
   return (
-    <main>
-      <form action={"/login"}>
-        <input type="email" name="email" placeholder="Email" />
-        <input type="password" name="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </main>
+    <form className={styles.form} action={"/login"}>
+      <label htmlFor="email">Email Address</label>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="example@gmail.com"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        placeholder="********"
+      />
+      <div>
+        <input type="checkbox" id="remember-me" />
+        <label htmlFor="remember-me">Remember me</label>
+      </div>
+      <button className={styles.btn} type="submit">
+        Login
+      </button>
+    </form>
   );
 }
