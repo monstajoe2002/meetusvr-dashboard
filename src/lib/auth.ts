d6343200392c0b56e7f3a6b4c866436e8eaa268f
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-09 23:10:50 +0200

    redirect user instead of returning an object

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,21 +1,20 @@
+import { redirect } from "next/navigation";
+
 export const login = async (
   email: string,
   password: string,
   isEmployee: boolean
 ) => {
-  const response = await fetch(
-    "https://api-yeshtery.dev.meetusvr.com/v1/yeshtery/token",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        password,
-        isEmployee,
-      }),
-    }
-  );
-  return response.json();
+  await fetch("https://api-yeshtery.dev.meetusvr.com/v1/yeshtery/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+      isEmployee,
+    }),
+  });
+  redirect("/");
 };
