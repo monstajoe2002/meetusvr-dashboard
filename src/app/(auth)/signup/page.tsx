1bdff0c385c7b95b290ee13031705d61675718e4
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-09 14:56:02 +0200

    adjust max height

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,26 @@
 "use client";
-
+import styles from "../auth.module.css";
 export default function SignupPage() {
-  return <div>sign up</div>;
+  return (
+    <form className={styles.form} action={"/login"}>
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" placeholder="Input your name here" />
+      <label htmlFor="email">Email Address</label>
+      <input
+        type="email"
+        name="email"
+        required
+        placeholder="example@gmail.com"
+      />
+      <label htmlFor="password">Password</label>
+      <input type="password" name="password" required placeholder="********" />
+      <div>
+        <input type="checkbox" id="remember-me" />
+        <label htmlFor="remember-me">Remember me</label>
+      </div>
+      <button className={styles.btn} type="submit">
+        Signup
+      </button>
+    </form>
+  );
 }
