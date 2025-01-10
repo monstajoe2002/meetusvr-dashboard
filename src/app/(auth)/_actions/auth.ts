6c90ad93ab132c0dd3c3797b186bf128215b956f
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-10 02:01:37 +0200

    refactor: move auth to _actions folder

--- src/app/(auth)/_actions/auth.ts
+++ src/app/(auth)/_actions/auth.ts
@@ -1,8 +1,14 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
 export const login = async (
   email: string,
   password: string,
   isEmployee: boolean
 ) => {
+  const cookieStore = await cookies();
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/yeshtery/token`, {
     method: "POST",
     headers: {
@@ -14,5 +20,7 @@ export const login = async (
       isEmployee,
     }),
   });
-  return res.json();
+  const data = await res.json();
+  cookieStore.set("token", data.token, { httpOnly: true });
+  redirect("/");
 };
