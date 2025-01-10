4baca969c6e9751c85fab2f52d620cb49e1fe796
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-10 19:11:58 +0200

    style: add bottom bar component with avatar styling

--- src/components/bottom-bar.tsx
+++ src/components/bottom-bar.tsx
@@ -3,8 +3,9 @@ import React from "react";
 import "../app/globals.css";
 export const BottomBar = () => {
   return (
-    <div>
+    <div className="bottom-bar">
       <Image
+        className="avatar"
         src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
         alt="avatar"
         width={50}
