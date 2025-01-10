d311ce9a511050fd39ebef3d8218b3cc0b976424
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-10 18:27:15 +0200

    create a home button with styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { user } from "@/lib/auth";
 import { signout } from "./(auth)/_actions/auth";
 import styles from "./dashboard.module.css";
+import Link from "next/link";
 export default async function Home() {
   const userInfo = await user();
   return (
@@ -10,6 +11,7 @@ export default async function Home() {
         Welcome to a world where virtual reality redefines experiences – a place
         where shopping and events converge to create an extraordinary journey.
       </p>
+      <button className={styles.homeButton}>Home</button>
       <button onClick={signout}>Sign out</button>
     </main>
   );
