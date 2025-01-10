0ce342a4bb4e62637fb505da54f4be5d076807b1
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-10 15:26:53 +0200

    style: add dashboard styles and enhance page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { user } from "@/lib/auth";
 import { signout } from "./(auth)/_actions/auth";
-
+import styles from "./dashboard.module.css";
 export default async function Home() {
   const userInfo = await user();
   return (
     <main>
-      <h1>Welcome aboard, {userInfo.name}</h1>
+      <h1 className={styles.heading}>Good Evening, {userInfo.name}</h1>
+      <p className={styles.description}>
+        Welcome to a world where virtual reality redefines experiences – a place
+        where shopping and events converge to create an extraordinary journey.
+      </p>
       <button onClick={signout}>Sign out</button>
     </main>
   );
