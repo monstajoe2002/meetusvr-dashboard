9b9db6404b6105c60b55bc66b6a3cd7bbc32a8ef
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-10 03:35:39 +0200

    add middleware for authentication

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const cookie = request.cookies.get("meetus-user")?.value;
+  if (!cookie && request.nextUrl.pathname !== "/login") {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+  if (cookie && request.nextUrl.pathname === "/login") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
