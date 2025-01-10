dc0105ece74a2f29a92e36b50cc73793e73a1a8a
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-10 19:02:54 +0200

    style: center align headings and descriptions in dashboard, adjust avatar size in bottom bar

--- src/components/bottom-bar.tsx
+++ src/components/bottom-bar.tsx
@@ -7,8 +7,8 @@ export const BottomBar = () => {
       <Image
         src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
         alt="avatar"
-        width={30}
-        height={30}
+        width={50}
+        height={50}
       />
     </div>
   );
