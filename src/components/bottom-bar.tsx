2e2cca16ea728a4b857412112b466678603afcfb
Youssef Amr El-Shehaby <monstajoe.elshehaby@proton.me>
2025-01-10 20:40:07 +0200

    style: enhance bottom bar with new navigation buttons and improved layout

--- src/components/bottom-bar.tsx
+++ src/components/bottom-bar.tsx
@@ -1,22 +1,62 @@
 import Image from "next/image";
 import React from "react";
-import styles from "./bottomBar.module.css";
+import bottomBarStyles from "./bottomBar.module.css";
+import navButtonStyles from "./navButton.module.css";
 import { BiSolidBattery } from "react-icons/bi";
 import { FaWifi } from "react-icons/fa";
+import { NavButton } from "./nav-button";
+import { HiHome } from "react-icons/hi2";
+import { HiSpeakerWave } from "react-icons/hi2";
+import { FaBasketShopping } from "react-icons/fa6";
+import { BsFillChatSquareDotsFill } from "react-icons/bs";
+import { BsCalendarEventFill } from "react-icons/bs";
+import { RiLayoutGrid2Fill } from "react-icons/ri";
 export const BottomBar = () => {
   return (
-    <div className={styles.bottomBar}>
+    <div className={bottomBarStyles.bottomBar}>
       <Image
-        className={styles.avatar}
+        className={bottomBarStyles.avatar}
         src="https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
         alt="avatar"
         width={50}
         height={50}
       />
-      <p>10:28 PM</p>
+      <p style={{ width: "max-content" }}>10:28 PM</p>
       <BiSolidBattery size={25} />
       <FaWifi size={25} />
       <p>28°C</p>
+      <div style={{ gap: "35px", display: "flex" }}>
+        <NavButton
+          label="Home"
+          icon={HiHome}
+          className={navButtonStyles.homeButton}
+        />
+        <NavButton
+          label="Sound"
+          icon={HiSpeakerWave}
+          className={navButtonStyles.soundButton}
+        />
+        <NavButton
+          label="Product"
+          icon={FaBasketShopping}
+          className={navButtonStyles.productButton}
+        />
+        <NavButton
+          label="Assistant"
+          icon={BsFillChatSquareDotsFill}
+          className={navButtonStyles.assistantButton}
+        />
+        <NavButton
+          label="Event"
+          icon={BsCalendarEventFill}
+          className={navButtonStyles.eventButton}
+        />
+        <NavButton
+          label="Menu"
+          icon={RiLayoutGrid2Fill}
+          className={navButtonStyles.menuButton}
+        />
+      </div>
     </div>
   );
 };
--- src/components/nav-button.tsx
+++ src/components/nav-button.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import styles from "./navButton.module.css";
+import { IconBaseProps } from "react-icons";
+interface NavButtonProps extends IconBaseProps {
+  icon: React.ComponentType<IconBaseProps>;
+  label: string;
+  className?: string;
+}
 
-export const NavButton = () => {
-  return <div>NavButton</div>;
+export const NavButton = ({ icon: Icon, label, className }: NavButtonProps) => {
+  return (
+    <div>
+      <button className={`${styles.navButton} ${className}`}>
+        <Icon size="24" />
+      </button>
+      <p className={styles.navLabel}>{label}</p>
+    </div>
+  );
 };
